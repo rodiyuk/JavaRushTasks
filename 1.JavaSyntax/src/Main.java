1a07de0b9cf2a60efb57a7e40635e14b3081a79c
Dimmi <sinrul86@gmail.com>
2020-08-21 17:57:57 +0300

    from home

--- 1.JavaSyntax/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main
-{
-    public static void main(String[] args) {
-        String s1 = "asdfghqwezxc";
-        String s2 = "asdfghghjjfqwezxc";
-        System.out.println(s1.compareTo(s2));
-    }
-}
