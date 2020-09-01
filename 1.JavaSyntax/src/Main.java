50b49ad9306106157630e959dfe67ecc97122b9a
Dimmi <sinrul86@gmail.com>
2020-09-01 23:23:16 +0300

    01/09 from home

--- 1.JavaSyntax/src/Main.java
+++ 1.JavaSyntax/src/Main.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 import java.nio.file.*;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        Path path1 = Path.of("c://test1//test01//test001//test.txt");
-        Path path2 = Path.of("c://test2");
-        System.out.println(path2.resolve(path2.relativize(path1)));
+        System.out.println(LocalDate.of(1971, 1, 1).toEpochDay());
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1612/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task16.task1612;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Map<LocalDate, List<LocalTime>> dateMap = DateTimeGenerator.generateDateMap();
+        printCollection(dateMap.entrySet());
+
+        Set<LocalDateTime> dateSet = convert(dateMap);
+        printCollection(dateSet);
+    }
+
+    static Set<LocalDateTime> convert(Map<LocalDate, List<LocalTime>> sourceMap) {
+        //напишите тут ваш код
+        Set<LocalDateTime> dateTimeSet = new HashSet<>();
+        for (Map.Entry<LocalDate, List<LocalTime>> pair: sourceMap.entrySet()){
+            for (int i = 0; i < pair.getValue().size(); i++) {
+                dateTimeSet.add(LocalDateTime.of(pair.getKey(),pair.getValue().get(i)));
+            }
+
+        }
+        return dateTimeSet;
+    }
+
+    static void printCollection(Collection<?> collection) {
+        System.out.println("-----------------------------------------------------");
+        collection.forEach(System.out::println);
+    }
+}
