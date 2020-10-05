ffc5b6163a3aae2bf1d20b6366d59cbf94169c13
rodiyuk <dmitryrodiyuk@gmail.com>
2020-10-05 15:36:15 +0300

    from work 05/10

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -1,16 +1,18 @@
 package com.javarush.task.task19.task1918;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /* 
 Знакомство с тегами
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String tag = args[0];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine().replaceAll("\n\t", "");
+        System.out.println(line.substring(line.indexOf("<" + tag), line.indexOf("</" + tag) + tag.length()+3));
     }
 }
