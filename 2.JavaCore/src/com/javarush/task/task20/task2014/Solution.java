93a41a133a84d56d1d15d079c2b095604fbb4347
rodiyuk <dmitryrodiyuk@gmail.com>
2020-10-05 16:05:55 +0300

    from work 05/10

--- 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task20.task2014;
 
-import java.io.Serializable;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,13 +8,31 @@ import java.util.Date;
 Serializable Solution
 */
 public class Solution implements Serializable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream("in.txt");
+        FileInputStream inputStream = new FileInputStream("in.txt");
+        Solution savedObject = new Solution(5);
+        savedObject.save(outputStream);
+        Solution loadedObject = new Solution(15);
+        loadedObject.load(inputStream);
+        System.out.println(savedObject.string.equals(loadedObject.string));
         System.out.println(new Solution(4));
     }
+    public void save(OutputStream outputStream) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(outputStream));
+        bufferedWriter.write(string);
+        bufferedWriter.close();
+    }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    public void load(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        string = bufferedReader.readLine();
+        bufferedReader.close();
+    }
+
+    transient private final String pattern = "dd MMMM yyyy, EEEE";
+    transient private Date currentDate;
+    transient private int temperature;
     String string;
 
     public Solution(int temperature) {
