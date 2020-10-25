2b334c37614f02ddfd589b3df23a8c6ab6f25687
Dimmi <sinrul86@gmail.com>
2020-10-25 23:45:46 +0300

    25/10 from home

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public int hash() {
+        return 23;
+    }
+
+    public void equal() {
+    }
+}
